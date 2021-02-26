65dd123c2f2c013e8495dacb9750811b7b1a7af1
Tunahan <tunahanaydinoglu@outlook.com>
2021-02-26 14:46:55 +0300

    add insert method

--- Week5.DapperAPI/Controllers/DapperSampleController.cs
+++ Week5.DapperAPI/Controllers/DapperSampleController.cs
@@ -26,7 +26,7 @@ namespace Week5.DapperAPI.Controllers
         public IActionResult DapperSelect()
         {
             /*
-             Person.Person tablosundaki bütün verileri listelemek için bir sql komutu oluşturdum ve Query methodu yardımıyla 
+             Databasedeki Person.Person tablosundaki bütün verileri listelemek için bir sql komutu oluşturdum ve Query methodu yardımıyla 
             Databasede sql komutumu çalıştırmış oldum. Geriye dönen datayı Person classıma map ettim ve kullanıcıların ıd, firtname, lastname datalarını
             kullanıma hazır şekilde databaseden çekmiş oldum.
             Sql komutunun profilerdaki hali : select * from [Person].[Person]
@@ -40,5 +40,29 @@ namespace Week5.DapperAPI.Controllers
             }
                 return Ok(persons);
         }
+
+
+        public IActionResult DapperInsert()
+        {
+            /*
+                Databaseimde test için dbo.TestPerson adında bir tablo oluşturdum.
+            insert into komutuyla sqlde ekleme işlemleri yapabilecek bir sql komutu yazdım ve values tarafında göndereceğim parametleri belirttim.
+            TestPerson classımdan bir testData nesnesi oluşturdum.
+            dapperın execute komutuna sql sorgumu ve parametre olarak nesnemi gönderdim ve dapper nesnemin parametleriyle sqldeki parametleri eşleyip 
+            databasede insert işlemini gerçekleştirdi .
+            
+             */
+            
+            using (IDbConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                string sql = @"insert into dbo.TestPerson (Name, Surname) values (@Name, @Surname);";
+
+                TestPerson testData = new TestPerson { Name = "Tuna", Surname = "Aydinoglu" };
+                var affected = db.Execute(sql, testData);
+            }
+                return Ok();
+        }
+
+
     }
 }
