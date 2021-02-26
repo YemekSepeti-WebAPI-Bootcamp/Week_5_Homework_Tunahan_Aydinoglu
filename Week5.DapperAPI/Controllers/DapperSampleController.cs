e75ea0aed56cbff5900ca5eb4b8e81cab63f711b
Tunahan <tunahanaydinoglu@outlook.com>
2021-02-26 13:17:11 +0300

    Initial commit

---
+++ Week5.DapperAPI/Controllers/DapperSampleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week5.DapperAPI.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class DapperSampleController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DapperSampleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
